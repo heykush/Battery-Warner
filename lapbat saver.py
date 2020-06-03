6d30239eeb971747f8950c96fc13a56d55f80eec
Gaurav kushwaha <57096457+heykush@users.noreply.github.com>
2020-06-03 23:49:25 +0530

    Add files via upload

---
+++ lapbat saver.py
@@ -0,0 +1,24 @@
+import os ,sys
+from time import sleep
+from plyer import notification
+from psutil import sensors_battery
+
+percentage_trigger = 95
+
+def notify():
+    notification.notify(
+        title='Hey!, Unplug your Charger!',
+        message='Your battery is already charged',
+        app_icon=r"C:\Users\gkush\Downloads\c.ico",
+        timeout=10,
+    )
+
+if __name__ == "__main__":
+    while True:
+        battery = sensors_battery()
+        percent = battery.percent
+        power_plugged = battery.power_plugged
+        codition=[percent > percentage_trigger and power_plugged]
+        if all(codition):
+            notify()
+    sleep(60*3)
